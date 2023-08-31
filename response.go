c697da6bff9697677ee00d0aab8ef01814d5ad52
Andy Day <aday@fabius.io>
2023-08-30 19:57:48 -0700

    Add stop field to Response (#8)
    Doesn't yet appear in the documentation but was made aware of it's existence by Anthropic support.

--- response.go
+++ response.go
@@ -6,6 +6,8 @@ type Response struct {
 	Completion string `json:"completion"`
 	// StopReason is the reason Anthropic stopped sampling. It will be one of "stop_sequence" or "max_tokens".
 	StopReason *string `json:"stop_reason"`
+	// Stop is the stop sequence that caused the model to stop sampling.
+	Stop *string `json:"stop"`
 	// Model is the model that performed the completion.
 	Model Model `json:"model"`
 }
