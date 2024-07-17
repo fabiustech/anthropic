80989b722ed4ab4a4ddc8c4e694798bc27fab997
Andy Day <aday@fabius.io>
2024-07-17 13:23:30 -0700

    Implement tools + tool use in the client
    This change implements tool use (function calling) in the client.
    
    See more here: https://docs.anthropic.com/en/docs/build-with-claude/tool-use

--- v3/message.go
+++ v3/message.go
@@ -19,12 +19,18 @@ type ShortHandMessage struct {
 
 // MessageContent represents the content of a message.
 type MessageContent struct {
-	// Type is the type of the content. It can be either "text" or "image".
+	// Type is the type of the content. It can be either "text", "image", or "tool_use".
 	Type string `json:"type"`
 	// Text is the text content of the message. Leave this empty if passing an image.
 	Text string `json:"text,omitempty"`
 	// Source is the media source of the message. Leave this empty if passing text.
 	Source *MediaSource `json:"source,omitempty"`
+	// Name is the name of the tool used (if any) .
+	Name string `json:"name,omitempty"`
+	// Input is the input of for a specificied tool (if any).
+	Input string `json:"input,omitempty"`
+	// Content is the result of a calling specified tool (if any).
+	Content string `json:"content,omitempty"`
 }
 
 // MediaSource represents the media source of a message.
---
+++ v3/tool.go
@@ -0,0 +1,136 @@
+package v3
+
+// Tool represents a tool that the model may use.
+type Tool struct {
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	InputSchema *Schema `json:"input_schema"`
+}
+
+// Schema represents a basic JSON schema.
+type Schema struct {
+	Type        SchemaType         `json:"type"`
+	Properties  map[string]*Schema `json:"properties,omitempty"`
+	Title       string             `json:"title,omitempty"`
+	Description string             `json:"description,omitempty"`
+	Required    []string           `json:"required,omitempty"`
+}
+
+// ToolChoice represents how the model should use the provided tools. The model can use a specific tool, any available
+// tool, or decide by itself.
+type ToolChoice struct {
+	// Type is the type of tool choice. Required.
+	Type string `json:"type"`
+	// Name is the name of the tool to use. Required if Type is "tool".
+	Name string `json:"name,omitempty"`
+}
+
+// ToolChoiceType represents the type of tool choice.
+type ToolChoiceType int
+
+const (
+	// ToolChoiceAuto allows Claude to decide whether to call any provided tools or not. This is the default value.
+	ToolChoiceAuto ToolChoiceType = iota
+	// ToolChoiceAny tells Claude that it must use one of the provided tools, but doesn’t force a particular tool.
+	ToolChoiceAny
+	// ToolChoiceTool allows us to force Claude to always use a particular tool.
+	ToolChoiceTool
+)
+
+// String implements the fmt.Stringer interface.
+func (t ToolChoiceType) String() string {
+	return toolChoiceTypeToString[t]
+}
+
+// MarshalText implements the encoding.TextMarshaler interface.
+func (t ToolChoiceType) MarshalText() ([]byte, error) {
+	return []byte(t.String()), nil
+}
+
+// UnmarshalText implements the encoding.TextUnmarshaler interface.
+// On unrecognized value, it sets |e| to ToolChoiceAuto (the default).
+func (t *ToolChoiceType) UnmarshalText(b []byte) error {
+	if val, ok := stringToToolChoiceType[(string(b))]; ok {
+		*t = val
+		return nil
+	}
+
+	*t = ToolChoiceAuto
+
+	return nil
+}
+
+var toolChoiceTypeToString = map[ToolChoiceType]string{
+	ToolChoiceAuto: "auto",
+	ToolChoiceAny:  "any",
+	ToolChoiceTool: "tool",
+}
+
+var stringToToolChoiceType = map[string]ToolChoiceType{
+	"auto": ToolChoiceAuto,
+	"any":  ToolChoiceAny,
+	"tool": ToolChoiceTool,
+}
+
+// SchemaType represents the type of a JSON schema.
+type SchemaType int
+
+const (
+	// SchemaTypeNull represents a null value.
+	SchemaTypeNull SchemaType = iota
+	// SchemaTypeString represents a string value.
+	SchemaTypeString
+	// SchemaTypeNumber represents a number value.
+	SchemaTypeNumber
+	// SchemaTypeInteger represents an integer value.
+	SchemaTypeInteger
+	// SchemaTypeObject represents an object value.
+	SchemaTypeObject
+	// SchemaTypeArray represents an array value.
+	SchemaTypeArray
+	// SchemaTypeBoolean represents a boolean value.
+	SchemaTypeBoolean
+)
+
+// String implements the fmt.Stringer interface.
+func (st SchemaType) String() string {
+	return schemaTypeToString[st]
+}
+
+// MarshalText implements the encoding.TextMarshaler interface.
+func (st SchemaType) MarshalText() ([]byte, error) {
+	return []byte(st.String()), nil
+}
+
+// UnmarshalText implements the encoding.TextUnmarshaler interface.
+// On unrecognized value, it sets |e| to Null.
+func (st *SchemaType) UnmarshalText(b []byte) error {
+	if val, ok := stringToSchemaType[(string(b))]; ok {
+		*st = val
+		return nil
+	}
+
+	*st = SchemaTypeNull
+
+	return nil
+}
+
+var schemaTypeToString = map[SchemaType]string{
+	SchemaTypeNull:    "null",
+	SchemaTypeString:  "string",
+	SchemaTypeNumber:  "number",
+	SchemaTypeInteger: "integer",
+	SchemaTypeObject:  "object",
+	SchemaTypeArray:   "array",
+	SchemaTypeBoolean: "boolean",
+}
+
+var stringToSchemaType = map[string]SchemaType{
+	"null":    SchemaTypeNull,
+	"string":  SchemaTypeString,
+	"number":  SchemaTypeNumber,
+	"integer": SchemaTypeInteger,
+	"object":  SchemaTypeObject,
+	"array":   SchemaTypeArray,
+	"boolean": SchemaTypeBoolean,
+}
