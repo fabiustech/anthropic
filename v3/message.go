2521cdef7980760dc4f5c2a9e5a3bbf012df8978
Andy Day <aday@fabius.io>
2024-07-18 16:13:28 -0700

    Implement tools + tool use in the client (#21)
    This change implements tool use (function calling) in the client.
    
    See more here: https://docs.anthropic.com/en/docs/build-with-claude/tool-use

--- v3/message.go
+++ v3/message.go
@@ -1,5 +1,7 @@
 package v3
 
+import "encoding/json"
+
 // Message represents a message sent to the API.
 type Message struct {
 	// Role is the role of the message sender.
@@ -19,12 +21,18 @@ type ShortHandMessage struct {
 
 // MessageContent represents the content of a message.
 type MessageContent struct {
-	// Type is the type of the content. It can be either "text" or "image".
+	// Type is the type of the content. It can be either "text", "image", or "tool_use".
 	Type string `json:"type"`
 	// Text is the text content of the message. Leave this empty if passing an image.
 	Text string `json:"text,omitempty"`
 	// Source is the media source of the message. Leave this empty if passing text.
 	Source *MediaSource `json:"source,omitempty"`
+	// Name is the name of the tool used (if any) .
+	Name string `json:"name,omitempty"`
+	// Input is the input of for a specified tool (if any).
+	Input json.RawMessage `json:"input,omitempty"`
+	// Content is the result of a calling specified tool (if any).
+	Content string `json:"content,omitempty"`
 }
 
 // MediaSource represents the media source of a message.
